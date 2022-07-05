5a4fddd97d66892cf26e9a250bf4ee1c6266ac81
mikeattah <mailenemona@gmail.com>
2022-07-06 00:24:44 +0100

    completed educative-nyc-weather-data-scatterplot

--- educative-nyc-weather-data-scatterplot.js
+++ educative-nyc-weather-data-scatterplot.js
@@ -1,7 +1,6 @@
 async function draw() {
   // Data
   const data = await d3.json("educative-nyc-weather-data-scatterplot.json");
-  console.log(data);
 
   // Accessor Functions
   // x/y humidity/apparentTemperature
@@ -44,7 +43,15 @@ async function draw() {
   const xScale = d3
     .scaleLinear()
     .domain(d3.extent(data, xAccessor))
-    .range([0, dimensions.ctrWidth]);
+    .rangeRound([0, dimensions.ctrWidth]) // round range to nearest integer
+    .clamp(true);
+
+  const yScale = d3
+    .scaleLinear()
+    .domain(d3.extent(data, yAccessor))
+    .rangeRound([dimensions.ctrHeight, 0])
+    .nice() // round domain to nearest whole number
+    .clamp(true);
 
   // Draw circles
   ctr
@@ -52,9 +59,41 @@ async function draw() {
     .data(data)
     .join("circle")
     .attr("cx", (d) => xScale(xAccessor(d)))
-    .attr("cy", yAccessor)
+    .attr("cy", (d) => yScale(yAccessor(d)))
     .attr("r", 5)
     .attr("fill", "red");
+
+  // Axes
+  const xAxis = d3
+    .axisBottom(xScale)
+    .ticks(5)
+    .tickFormat((d) => d * 100 + "%");
+  const yAxis = d3.axisLeft(yScale);
+
+  const xAxisGroup = ctr
+    .append("g")
+    .call(xAxis)
+    .style("transform", `translateY(${dimensions.ctrHeight}px)`)
+    .classed("axis", true);
+
+  const yAxisGroup = ctr.append("g").call(yAxis).classed("axis", true);
+
+  xAxisGroup
+    .append("text")
+    .attr("x", dimensions.ctrWidth / 2)
+    .attr("y", dimensions.margin.bottom - 10)
+    .attr("fill", "black")
+    .text("Humidity")
+    .style("text-anchor", "middle");
+
+  yAxisGroup
+    .append("text")
+    .attr("x", -dimensions.ctrHeight / 2)
+    .attr("y", -dimensions.margin.left + 15)
+    .attr("fill", "black")
+    .html("Temperature (&deg;F)")
+    .style("text-anchor", "middle")
+    .style("transform", "rotate(270deg)");
 }
 
 draw();
