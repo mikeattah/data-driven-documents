f57cb0783099d9444db207002e31b5070b408de5
mikeattah <mailenemona@gmail.com>
2022-07-08 10:17:45 +0100

    added transitions, mean line, and fixed animation bugs

--- educative-nyc-weather-data-histogram.js
+++ educative-nyc-weather-data-histogram.js
@@ -2,9 +2,6 @@ async function draw() {
   // Data
   const data = await d3.json("educative-nyc-weather-data-histogram.json");
 
-  const xAccessor = (d) => d.currently.humidity;
-  const yAccessor = (d) => d.length;
-
   // Dimensions
   let dimensions = {
     width: 800,
@@ -30,55 +27,124 @@ async function draw() {
       `translate(${dimensions.margins}, ${dimensions.margins})`
     );
 
-  // Scales
-  const xScale = d3
-    .scaleLinear()
-    .domain(d3.extent(data, xAccessor))
-    .range([0, dimensions.ctrWidth])
-    .nice();
+  // Create Labels
+  const labelsGroup = ctr.append("g").classed("bar-labels", true);
 
-  const bin = d3.bin().domain(xScale.domain()).value(xAccessor).threshold(10);
-
-  const newData = bin(data);
-
-  const yScale = d3
-    .scaleLinear()
-    .domain([0, d3.max(newData, yAccessor)])
-    .range([dimensions.ctrHeight, 0])
-    .nice();
-
-  // Draw Bars
-  ctr
-    .selectAll("rect")
-    .data(newData)
-    .join("rect")
-    .attr("width", (d) =>
-      d3.max([0, xScale(d.x1) - xScale(d.x0) - dimensions.padding])
-    )
-    .attr("height", (d) => dimensions.ctrHeight - yScale(yAccessor(d)))
-    .attr("x", (d) => xScale(d.x0))
-    .attr("y", (d) => yScale(yAccessor(d)))
-    .attr("fill", "#01c5c4");
-
-  // Add Labels
-  ctr
-    .append("g")
-    .classed("bar-labels", true)
-    .selectAll("text")
-    .data(newData)
-    .join("text")
-    .attr("x", (d) => xScale(d.x0) + (xScale(d.x1) - xScale(d.x0)) / 2)
-    .attr("y", (d) => yScale(yAccessor(d)) - 10)
-    .text(yAccessor);
-
-  // Draw Axis
-  const xAxis = d3.axisBottom(XScale);
-
+  // Create Axis
   const xAxisGroup = ctr
     .append("g")
     .style("transform", `translateY(${dimensions.ctrHeight}px)`);
 
-  xAxisGroup.call(xAxis);
+  // Create Mean Line
+  const meanLine = ctr.append("line").classed("mean-line", true);
+
+  function histogram(metric) {
+    const xAccessor = (d) => d.currently[metric],
+      yAccessor = (d) => d.length,
+      mean = d3.mean(data, xAccessor);
+
+    // Draw Mean Line
+    meanLine
+      .raise()
+      .transition(updateTransition)
+      .attr("x1", xScale(mean))
+      .attr("y1", 0)
+      .attr("x2", xScale(mean))
+      .attr("y2", dimensions.ctrHeight);
+
+    // Create Scales
+    const xScale = d3
+      .scaleLinear()
+      .domain(d3.extent(data, xAccessor))
+      .range([0, dimensions.ctrWidth])
+      .nice();
+
+    const bin = d3
+      .bin()
+      .domain(xScale.domain())
+      .value(xAccessor)
+      .thresholds(10);
+
+    const newData = bin(data);
+
+    const yScale = d3
+      .scaleLinear()
+      .domain([0, d3.max(newData, yAccessor)])
+      .range([dimensions.ctrHeight, 0])
+      .nice();
+
+    // Draw Bars
+    const exitTransition = d3.transition().duration(500);
+    const updateTransition = exitTransition.transition().duration(500);
+
+    ctr
+      .selectAll("rect")
+      .data(newData)
+      .join(
+        (enter) =>
+          enter
+            .append("rect")
+            .attr("width", (d) =>
+              d3.max([0, xScale(d.x1) - xScale(d.x0) - dimensions.padding])
+            )
+            .attr("height", 0)
+            .attr("x", (d) => xScale(d.x0))
+            .attr("y", dimensions.ctrHeight)
+            .attr("fill", "#b8de6f"),
+        (update) => update,
+        (exit) =>
+          exit
+            .attr("fill", "#f39233")
+            .transition(exitTransition)
+            .attr("y", dimensions.ctrHeight)
+            .attr("height", 0)
+            .remove()
+      )
+      .transition(updateTransition)
+      .attr("width", (d) =>
+        d3.max([0, xScale(d.x1) - xScale(d.x0) - dimensions.padding])
+      )
+      .attr("height", (d) => dimensions.ctrHeight - yScale(yAccessor(d)))
+      .attr("x", (d) => xScale(d.x0))
+      .attr("y", (d) => yScale(yAccessor(d)))
+      .attr("fill", "#01c5c4");
+
+    // Add Labels
+    labelsGroup
+      .selectAll("text")
+      .data(newData)
+      .join(
+        (enter) =>
+          enter
+            .append("text")
+            .attr("x", (d) => xScale(d.x0) + (xScale(d.x1) - xScale(d.x0)) / 2)
+            .attr("y", dimensions.ctrHeight)
+            .text(yAccessor),
+        (update) => update,
+        (exit) =>
+          exit
+            .transition(exitTransition)
+            .attr("y", dimensions.ctrHeight)
+            .remove()
+      )
+      .transition(updateTransition)
+      .attr("x", (d) => xScale(d.x0) + (xScale(d.x1) - xScale(d.x0)) / 2)
+      .attr("y", (d) => yScale(yAccessor(d)) - 10)
+      .text(yAccessor);
+
+    // Draw Axis
+    const xAxis = d3.axisBottom(xScale);
+
+    xAxisGroup.transition().call(xAxis);
+  }
+
+  d3.select("#metric").on("change", function (e) {
+    e.preventDefault();
+
+    histogram(this.value);
+  });
+
+  histogram("humidity");
 }
 
 draw();
