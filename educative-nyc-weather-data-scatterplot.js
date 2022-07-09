5d643163d569a8ba07675e83246d5c2fb3c0eb0f
mikeattah <mailenemona@gmail.com>
2022-07-09 02:17:37 +0100

    added voronoi diagram to scatterplot

--- educative-nyc-weather-data-scatterplot.js
+++ educative-nyc-weather-data-scatterplot.js
@@ -39,6 +39,9 @@ async function draw() {
       `translate(${dimensions.margin.left}, ${dimensions.margin.top})`
     );
 
+  // Create tooltip
+  const tooltip = d3.select("#tooltip");
+
   // Scales
   const xScale = d3
     .scaleLinear()
@@ -60,6 +63,7 @@ async function draw() {
     .join("circle")
     .attr("cx", (d) => xScale(xAccessor(d)))
     .attr("cy", (d) => yScale(yAccessor(d)))
+    .attr("data-temp", yAccessor)
     .attr("r", 5)
     .attr("fill", "red");
 
@@ -68,15 +72,12 @@ async function draw() {
     .axisBottom(xScale)
     .ticks(5)
     .tickFormat((d) => d * 100 + "%");
-  const yAxis = d3.axisLeft(yScale);
 
   const xAxisGroup = ctr
     .append("g")
     .call(xAxis)
     .style("transform", `translateY(${dimensions.ctrHeight}px)`)
     .classed("axis", true);
-
-  const yAxisGroup = ctr.append("g").call(yAxis).classed("axis", true);
 
   xAxisGroup
     .append("text")
@@ -86,6 +87,10 @@ async function draw() {
     .text("Humidity")
     .style("text-anchor", "middle");
 
+  const yAxis = d3.axisLeft(yScale);
+
+  const yAxisGroup = ctr.append("g").call(yAxis).classed("axis", true);
+
   yAxisGroup
     .append("text")
     .attr("x", -dimensions.ctrHeight / 2)
@@ -94,6 +99,62 @@ async function draw() {
     .html("Temperature (&deg;F)")
     .style("text-anchor", "middle")
     .style("transform", "rotate(270deg)");
+
+  const delaunay = d3.Delaunay.from(
+    data,
+    (d) => xScale(xAccessor(d)),
+    (d) => yScale(yAccessor(d))
+  );
+
+  const voronoi = delaunay.voronoi();
+  voronoi.xmax = dimensions.ctrWidth;
+  voronoi.ymax = dimensions.ctrHeight;
+
+  // Draw Partitions
+  ctr
+    .append("g")
+    .selectAll("path")
+    .data(data)
+    .join("path")
+    // .attr("stroke", "black")
+    .attr("fill", "transparent")
+    .attr("d", (d, i) => voronoi.renderCell(i))
+    .on("mouseenter", function (e, datum) {
+      ctr
+        .append("circle")
+        .classed("dot-hovered", true)
+        .attr("fill", "#120078")
+        .attr("r", 8)
+        .attr("cx", xScale(xAccessor(datum)))
+        .attr("cy", yScale(yAccessor(datum)))
+        .style("pointer-events", "none");
+
+      tooltip
+        .style("display", "block")
+        .style("top", yScale(yAccessor(datum)) - 25 + "px")
+        .style("left", xScale(xAccessor(datum)) + "px");
+
+      // formatters
+      const numberFormatter = d3.format(".2f");
+      const dateFormatter = d3.timeFormat("%B %-d, %Y");
+
+      tooltip
+        .select(".metric-humidity span")
+        .text(numberFormatter(xAccessor(datum)));
+
+      tooltip
+        .select(".metric-temp span")
+        .text(numberFormatter(yAccessor(datum)));
+
+      tooltip
+        .select(".metric-date")
+        .text(dateFormatter(datum.currently.time * 1000));
+    })
+    .on("mouseleave", function () {
+      d3.select(".dot-hovered").remove();
+
+      tooltip.style("display", "none");
+    });
 }
 
 draw();
