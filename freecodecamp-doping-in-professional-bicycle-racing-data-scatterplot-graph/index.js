30e61d8474b4ec9050c2a7a72e8596f317df4ec5
mikeattah <mailenemona@gmail.com>
2022-07-14 23:45:34 +0100

    FCCC scatterplot fixed y-axis orientation

--- freecodecamp-doping-in-professional-bicycle-racing-data-scatterplot-graph/index.js
+++ freecodecamp-doping-in-professional-bicycle-racing-data-scatterplot-graph/index.js
@@ -3,17 +3,19 @@ async function draw() {
   const data = await d3.json("data.json");
 
   // Formatters
+  const timeFormatter = d3.timeFormat("%M:%S");
 
   // Accessor Functions
   const xAccessor = (d) => d.Year;
   const yAccessor = (d) => d.Seconds; // problem area
+  const timeAccessor = (d) => d.Time;
   const nameAccessor = (d) => d.Name;
   const nationalityAccessor = (d) => d.Nationality;
   const dopingAccessor = (d) => d.Doping;
 
   // Dimensions
   let dimensions = {
-    width: 800,
+    width: 1000,
     height: 800,
     margins: 50,
   };
@@ -25,8 +27,6 @@ async function draw() {
   const svg = d3
     .select("#chart")
     .append("svg")
-    // .attr("width", dimensions.width)
-    // .attr("height", dimensions.height)
     .attr("viewBox", `0 0 ${dimensions.width} ${dimensions.height}`);
 
   // Chart Container
@@ -41,15 +41,17 @@ async function draw() {
   const xScale = d3
     .scaleLinear()
     .domain(d3.extent(data, xAccessor))
-    .range([0, dimensions.ctrWidth]);
-  // .clamp(true);
+    .range([0, dimensions.ctrWidth])
+    .clamp(true);
 
   const yScale = d3
     .scaleLinear()
     .domain(d3.extent(data, yAccessor))
-    .range([dimensions.ctrHeight, 0]);
-  // .nice() // round domain to nearest whole number
-  // .clamp(true);
+    .range([0, dimensions.ctrHeight])
+    .clamp(true);
+
+  // Tooltip
+  const tooltip = d3.select("#tooltip");
 
   // Circles
   ctr
@@ -62,18 +64,26 @@ async function draw() {
     .attr("data-xvalue", xAccessor)
     .attr("data-yvalue", yAccessor)
     .classed("dot", true)
-    .attr("fill", (d) => {
-      if (dopingAccessor(d) === "") {
-        return "green";
-      } else {
-        return "red";
-      }
-    })
+    .attr("fill", (d) => (dopingAccessor(d) === "" ? "#ff7f0e" : "#2463a5"))
     .attr("stroke", "black")
-    .attr("stroke-width", "1px");
+    .attr("stroke-width", "1px")
+    .on("mouseenter", function (e, datum) {
+      // Tooltip
+      tooltip
+        .style("display", "block")
+        .style("top", yScale(yAccessor(datum)) - 95 + "px")
+        .style("left", xScale(xAccessor(datum)) - 62 + "px");
 
-  // Tooltip
-  const tooltip = d3.select("#tooltip");
+      tooltip.select(".athlete-name").text(nameAccessor(datum) + ": ");
+      tooltip.select(".athlete-nationality").text(nationalityAccessor(datum));
+      tooltip.select(".date-year").text(" " + xAccessor(datum) + ",");
+      tooltip.select(".date-time").text(" " + timeAccessor(datum));
+      tooltip.select(".description").text(dopingAccessor(datum));
+    })
+    .on("mouseleave", function () {
+      d3.select(".dot-hovered").remove();
+      tooltip.style("display", "none");
+    });
 
   // Axes
   const xAxis = d3.axisBottom(xScale).tickFormat(d3.format("d"));
@@ -84,14 +94,6 @@ async function draw() {
     .style("transform", `translateY(${dimensions.ctrHeight}px)`)
     .attr("id", "x-axis")
     .classed("axis", true);
-
-  //   xAxisGroup
-  //     .append("text")
-  //     .attr("x", dimensions.ctrWidth / 2)
-  //     .attr("y", dimensions.margins - 10)
-  //     .attr("fill", "black")
-  //     .text("Humidity")
-  //     .style("text-anchor", "middle");
 
   const yAxis = d3.axisLeft(yScale);
 
@@ -109,62 +111,6 @@ async function draw() {
     .html("Time in Minutes")
     .style("text-anchor", "middle")
     .style("transform", "rotate(270deg)");
-
-  // Voronoi Diagrams
-  const delaunay = d3.Delaunay.from(
-    data,
-    (d) => xScale(xAccessor(d)),
-    (d) => yScale(yAccessor(d))
-  );
-
-  const voronoi = delaunay.voronoi();
-  voronoi.xmax = dimensions.ctrWidth;
-  voronoi.ymax = dimensions.ctrHeight;
-
-  // Voronoi Partitions
-  ctr
-    .append("g")
-    .selectAll("path")
-    .data(data)
-    .join("path")
-    // .attr("stroke", "black")
-    .attr("fill", "transparent")
-    .attr("d", (d, i) => voronoi.renderCell(i))
-    .on("mouseenter", function (e, datum) {
-      // Append Overlapping Circles
-      ctr
-        .append("circle")
-        .classed("dot-hovered", true)
-        .attr("r", 9)
-        .attr("cx", xScale(xAccessor(datum)))
-        .attr("cy", yScale(yAccessor(datum)))
-        .attr("fill", "#120078")
-        .attr("stroke", "black")
-        .attr("stroke-width", "1px")
-        .style("pointer-events", "none");
-
-      // formatters
-      // const numberFormatter = d3.format(".2f");
-      // const dateFormatter = d3.timeFormat("%B %-d, %Y");
-
-      // Tooltip
-      tooltip
-        .style("display", "block")
-        .style("top", yScale(yAccessor(datum)) - 55 + "px")
-        .style("left", xScale(xAccessor(datum)) - 27 + "px");
-
-      tooltip.select(".athlete-name").text(nameAccessor(datum) + ": ");
-
-      tooltip.select(".athlete-nationality").text(nationalityAccessor(datum));
-
-      tooltip.select(".date-year").text(" " + xAccessor(datum));
-
-      tooltip.select(".date-time").text(" " + yAccessor(datum));
-    })
-    .on("mouseleave", function () {
-      d3.select(".dot-hovered").remove();
-      tooltip.style("display", "none");
-    });
 }
 
 draw();
